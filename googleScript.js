f4145a1831b8e7d9f5db2ad5c404ec11787abd4c
shriharikallapur <57822273+shriharikallapur@users.noreply.github.com>
2024-12-26 21:32:25 +0530

    Update googleScript.js

--- googleScript.js
+++ googleScript.js
@@ -1,12 +1,15 @@
 $(document).on("click", ".searchBar-inner-2", function() {
-	$(".searchBar").css({"height": "400px",
-						"position": "absolute",
-						"background-color": "rgb(255, 255, 255)",
-						 "border-radius": "20px"});
-	$(".searcher-btn").css({"padding-top": "64px"});
-});
+	$(".searchBar-inner-container").css({
+		"height": "400px",
+		"position": "absolute",
+		"background-color": "rgb(255, 255, 255)",
+		"border-radius": "20px",
+		"top": "18rem"
+	})
+	$(".searcher-btn").css({"padding-top": "64px"})
+})
 
 function hiding() {
-	$(".searchBar").css({"height": "44px", "border-radius": "50px"});
-	$(".searcher-btn").css({"padding-top": "64px"});
+	$(".searchBar-inner-container").css({"height": "44px", "border-radius": "50px"})
+	$(".searcher-btn").css({"padding-top": "64px"})
 }
